df84e9c0f1f2ea9b4ddc084304a490d05be45ecb
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-01-26 09:06:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var secretNum;
 var clicks = 0;
 var theirGuess;
+var numOfGuesses = 0;
 function gameStart(){
   clicks += 1;
   if(clicks == 1){
     secretNum = Math.ceil(Math.random()*100);
     document.getElementById("theirGuess").style.visibility = "visible";
+    document.getElementById("btnPress").innerHTML = "Guess";
     console.log(secretNum);
   }else{
+    numOfGuesses += 1;
+    document.getElementById("guesses").innerHTML = "Guesses: " + numOfGuesses;
     theirGuess = document.getElementById("theirGuess").value;
     if(theirGuess > secretNum){
-      document.getElementById("instruction").innerHTML = "Too High"
+      document.getElementById("feedback").innerHTML = "Too High"
     }else if(theirGuess< secretNum){
-        document.getElementById("instruction").innerHTML = "Too Low"
+        document.getElementById("feedback").innerHTML = "Too Low"
     }else{
-      document.getElementById("instruction").innerHTML = "Got it!"
+      document.getElementById("feedback").innerHTML = "Got it!"
     }
   }//end else
 }// end gameStart
