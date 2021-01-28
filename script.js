a47806b9b62df53d24eab6883b3a704eb53b4e45
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-01-28 09:32:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,17 +14,21 @@ function gameStart(){
     console.log(secretNum);
   }else{
     numOfGuesses += 1;
+     document.getElementById("animatedGif").style.visibility = "hidden";
     document.getElementById("guesses").innerHTML = "Guesses: " + numOfGuesses;
     theirGuess = document.getElementById("theirGuess").value;
     if(theirGuess > secretNum){
       document.getElementById("feedback").innerHTML = "Too High"
+      checkRealClose()
       checkFail()
     }else if(theirGuess< secretNum){
         document.getElementById("feedback").innerHTML = "Too Low"
+        checkRealClose()
         checkFail()
     }else{
       document.getElementById("feedback").innerHTML = "Got it! </br> Play again?"
       document.getElementById("btnPress").innerHTML = "Play Again?";
+       document.getElementById("animatedGif").src = "gotIt.gif"
       document.getElementById("theirGuess").style.visibility = "hidden";
       clicks = 0;
       numOfGuesses = 0;
@@ -43,3 +47,12 @@ function checkFail(){
       numOfGuesses = 0;
   }//end if
 }//end function checkFail
+
+function checkRealClose(){
+  var difference ;
+  difference = Math.abs(secretNum - theirGuess);
+  if ( difference < 4){
+      document.getElementById("animatedGif").src = "soClose.jpg";
+      document.getElementById("animatedGif").style.visibility = "visible";
+  }
+}
